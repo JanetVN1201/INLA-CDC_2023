ae9267d4fb1b55e00e31f54b8ccba391a3e70dd0
JanetVN1201 <58976783+JanetVN1201@users.noreply.github.com>
2023-09-18 16:27:48 +0300

    Updates Libs and started material for Day 1

--- Libraries.R
+++ Libraries.R
@@ -8,7 +8,8 @@ libs = c("DiagrammeR","car","ggpubr","spdep","RColorBrewer",
          "spatstat","rgdal","sp","maptools","latticeExtra",
          "gridExtra","gstat","raster","ggplot2","ggfortify",
          "survival","joineR","BayesSurvival","icenReg","nloptr",
-         "faraway","lme4","boot")
+         "faraway","lme4","boot","sf","coda","spBayesSurv",
+         "BayesX", "R2BayesX", "fields")
 ix <- which(!sapply(libs, require, char = TRUE))
 if (length(ix) > 0) {install.packages(pkgs[ix], repos = "https://cloud.r-project.org/")
   sapply(pkgs[ix], require, char = TRUE)}
