d04e6b457609ae8da6988ae2761dd22b472eca05
JanetVN1201 <58976783+JanetVN1201@users.noreply.github.com>
2023-09-18 11:24:09 +0300

    Create Libraries.R

---
+++ Libraries.R
@@ -0,0 +1,15 @@
+##Libraries necessary for the course
+#Install INLA
+install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
+library("INLA")
+
+#Other libraries
+libs = c("DiagrammeR","car","ggpubr","spdep","RColorBrewer",
+         "spatstat","rgdal","sp","maptools","latticeExtra",
+         "gridExtra","gstat","raster","ggplot2","ggfortify",
+         "survival","joineR","BayesSurvival","icenReg","nloptr",
+         "faraway","lme4","boot")
+ix <- which(!sapply(libs, require, char = TRUE))
+if (length(ix) > 0) {install.packages(pkgs[ix], repos = "https://cloud.r-project.org/")
+  sapply(pkgs[ix], require, char = TRUE)}
+
