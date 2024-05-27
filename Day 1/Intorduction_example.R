aa465d5ffbf1b891102d80bad7bded72c8419db5
JanetVN1201 <58976783+JanetVN1201@users.noreply.github.com>
2024-05-27 10:26:43 -0400

    Finished Day 1 uploads

---
+++ Day 1/Intorduction_example.R
@@ -0,0 +1,19 @@
+library(INLA)
+Q_10 = INLA:::inla.rw1(10)
+diag(Q_10) <- diag(Q_10)+0.01
+Q_10
+Q_10_ns = matrix(Q_10, nrow = 10, byrow = T)
+Q_10_ns
+S_10 = SparseM::solve(Q_10)
+S_10
+S_10_ns = solve(Q_10_ns)
+S_10_ns
+
+
+##Create your own
+i <- c(1,2,3,4,8)
+j <- c(2:6)
+x <- 1:5
+A <- sparseMatrix(i, j, x = x)
+A
+summary(A)
